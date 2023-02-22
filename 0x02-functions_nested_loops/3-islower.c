fe5cbeb8d88f2f19767ee79c67e108e2a437ad4d
EwuraAmaSam <ewura.sam@ashesi.edu.gh>
2023-02-21 16:44:25 -0800

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - function that prints lower case
+ * @c: parameter
+ * Return: 1 if lower case
+ * and 0 if upper
+ */
+int _islower(int c)
+{
+	if (c >= 97 and c <= 122)
+		return (1);
+	else
+		return (0);
+
+}

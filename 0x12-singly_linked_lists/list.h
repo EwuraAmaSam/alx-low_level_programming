db38916df749b8171d36e8eecaebb479a18d749d
EwuraAmaSam <ewura.sam@ashesi.edu.gh>
2023-03-31 15:13:50 -0700

    Prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -6,7 +6,6 @@
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
- *
  * Description: singly linked list node structure
  * for Holberton project
  */
@@ -15,6 +14,7 @@ typedef struct list_s
 	char *str;
 	unsigned int len;
 	struct list_s *next;
+
 } list_t;
 
 size_t print_list(const list_t *h);

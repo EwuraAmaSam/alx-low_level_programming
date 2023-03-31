944058ccd8ad42e7f0e6fd2994783e598f818cd1
EwuraAmaSam <ewura.sam@ashesi.edu.gh>
2023-03-31 16:29:15 -0700

    Advanced task -Prints sentences

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void print_sentence(void)_attribute_((constructor));
+
+/**
+ * print_sentence - prints the sentence
+ */
+
+void print_sentence(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

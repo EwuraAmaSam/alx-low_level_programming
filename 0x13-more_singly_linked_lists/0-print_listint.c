91a8950cb7bb773792a27f35645f6b35ffb7ad66
EwuraAmaSam <ewura.sam@ashesi.edu.gh>
2023-04-04 16:52:01 -0700

    Prints element of a list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "list.h"
+
+/**
+ * listint_len - returns the length
+ * @h: pointer to the linked list
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t init = 0;
+
+	while(h != NULL)
+	{
+		printf("%d\n", h -> n);
+		h = h -> next;
+		init++;
+	}
+	return (init);
+}

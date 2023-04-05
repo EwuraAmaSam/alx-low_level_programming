66b9444b92764bf563e6d3967bfae8028de07ce3
EwuraAmaSam <ewura.sam@ashesi.edu.gh>
2023-04-05 07:05:51 -0700

    Fix betty error

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,10 +10,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *init = head;
 
-	for (unsigned int i = 0; init != NULL; && i < index; i++)
+	for (unsigned int i = 0; init != NULL; &&i < index; i++)
 		init = init->next;
 
-	return (init == NULL) ? NULL : init;
+	return ((init == NULL) ? NULL : init);
 
 }
 

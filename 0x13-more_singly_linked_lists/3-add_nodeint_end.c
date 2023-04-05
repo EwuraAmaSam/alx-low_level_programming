4062291cb808c7e78e533c8237d55952fc097fde
EwuraAmaSam <ewura.sam@ashesi.edu.gh>
2023-04-05 05:01:56 -0700

    Fixing conditionals

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,10 +13,21 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *init = *head;
 
 	num = malloc(sizeof(listint_t));
-	if (!num)
+	if (h)
 		return (NULL);
 
 	num->n = n;
 	num->next = NULL;
+	if (*head == NULL)
+	{
+		*head = num;
+		return (num);
+	}
+
+	while (init->next)
+		init = init->next;
+	init->next = num;
+
+	return (num);
 }
 

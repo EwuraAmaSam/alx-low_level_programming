be4bd52df6dc47f41cddf1ee629158c655b855b9
EwuraAmaSam <ewura.sam@ashesi.edu.gh>
2023-04-04 18:35:46 -0700

    Add nodes

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,7 +10,7 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *num;
-	listint_t *init = head;
+	listint_t *init = *head;
 
 	num = malloc(sizeof(listint_t));
 	if (!num)

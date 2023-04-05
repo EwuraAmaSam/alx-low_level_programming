3344a2fbb8b33926c4e0819fbcdeecdf4221903a
EwuraAmaSam <ewura.sam@ashesi.edu.gh>
2023-04-04 17:15:44 -0700

    Update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,18 @@
-#include "lists.h"
-
+#include "list.h"
 /**
- * print_listint - prints all the elements of a linked list
- * @h: linked list of type listint_t to print
- *
+ * listint_len - returns the length
+ * @h: pointer to the linked list
  * Return: number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
- size_t num = 0;
+	size_t init = 0;
 
- while (h)
- {
- printf("%d\n", h->n);
- num++;
- h = h->next;
- }
-
- return (num);
+	while (h != NULL)
+	{
+		h = h -> next;
+		init++;
+	}
+	return (init);
 }

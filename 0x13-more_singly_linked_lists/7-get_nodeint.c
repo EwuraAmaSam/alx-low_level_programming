bb90f58dbdab1323ff1339830288ae0701b4f2bb
EwuraAmaSam <ewura.sam@ashesi.edu.gh>
2023-04-05 07:14:24 -0700

    Deleted swp and swo

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,12 +8,11 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *init = head;
+        listint_t *init = head;
 
-	for (unsigned int i = 0; init != NULL; &&i < index; i++)
-		init = init->next;
+        for (unsigned int i = 0; init != NULL; &&i < index; i++)
+                init = init->next;
 
-	return ((init == NULL) ? NULL : init);
+        return ((init == NULL) ? NULL : init);
 
 }
-

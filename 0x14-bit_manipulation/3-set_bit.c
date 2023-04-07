b203802e29643fb0217182b524e12b46ebf17591
EwuraAmaSam <ewura.sam@ashesi.edu.gh>
2023-04-07 14:42:30 -0700

    Sets bit to 1

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_bit - sets to 1
+ * @n: pointer to a number
+ * @index: the index of the bit
+ */
+
+int set_bit(unsigned long int int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	*n |= 1 << index;
+
+	return (1);
+}
+

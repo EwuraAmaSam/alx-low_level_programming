6939af47913c086a2c194e1879870945bba38ba1
EwuraAmaSam <ewura.sam@ashesi.edu.gh>
2023-04-07 14:46:21 -0700

    Fixes

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -6,7 +6,7 @@
  * @index: the index of the bit
  */
 
-int set_bit(unsigned long int int *n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);

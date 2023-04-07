45a66bf6463bdb76188a3d2825e7dcfa246f302f
EwuraAmaSam <ewura.sam@ashesi.edu.gh>
2023-04-07 14:01:26 -0700

    Correction

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,14 +2,14 @@
 
 /**
  * print_binary - prints binary representation
- * @num: what to print
+ * @n: what to print
  * Return: void 
  */
 
 void print_binary(unsigned long int n)
 {
-	if (num > 1)
-		print_binary(num >> 1)
-	_putchar((num & 1) + '0');
+	if (n > 1)
+		print_binary(n >> 1)
+	_putchar((n & 1) + '0');
 }
 
